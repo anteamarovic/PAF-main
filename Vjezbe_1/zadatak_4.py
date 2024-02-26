e8212436c943d049dc65bd1480bb3c68b97a9cc9
anteamarovic <amarovic1@pmfst.hr>
2024-02-26 15:19:02 +0100

    commit

---
+++ Vjezbe_1/zadatak_4.py
@@ -0,0 +1,22 @@
+def pravac(x3, y3, x4, y4):
+    if x4 - x3 == 0:
+        print("Vertikalna linija, x = {}".format(x3))
+        return None
+    
+    a = (y4 - y3) / (x4 - x3)
+    b = y4 - x4 * a
+    return a, b
+
+koeficijenti = pravac(-1, 3, 3, -1)
+
+if koeficijenti is not None:
+    a, b = koeficijenti
+
+    if a == 0:
+        print('y = {}'.format(b))
+    elif b == 0:
+        print('y = {}x'.format(a))
+    elif b < 0:
+        print('y = {}x {}'.format(a, b))
+    else:
+        print('y = {}x + {}'.format(a, b))
