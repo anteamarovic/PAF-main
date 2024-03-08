c1ef19b9f67b8fc832c41cf074440d340da605f5
anteamarovic <amarovic1@pmfst.hr>
2024-03-08 16:07:26 +0100

    commit

--- Vjezbe_2/zadatak_2.py
+++ Vjezbe_2/zadatak_2.py
@@ -1,62 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-# Početni uvjeti
-v0 = 100  # početna brzina u m/s
-kut = 45  # kut otklona u stupnjevima
+v0 = 33
+kut = 53
+kut_radiani = kut * (math.pi / 180)
+ay = -9.81
+ax = 0
+vx = v0 * math.cos(kut_radiani)
+vy = v0 * math.sin(kut_radiani)
+lista_vx = []
+lista_vy = []
+lista_x = []
+lista_y = []
+lista_vremena = []
+x = 0
+y = 0
+t = 0
+dt = 0.01
 
-# Pretvaranje kuta u radiane
-kut_radiani = np.radians(kut)
-
-# Komponente ubrzanja
-ay = -9.81  # vertikalna komponenta akceleracije (gravitacija)
-ax = 0      # horizontalna komponenta akceleracije (brzina je konstantna)
-
-# Početne brzine
-vx = v0 * np.cos(kut_radiani)
-vy = v0 * np.sin(kut_radiani)
-
-# Inicijalizacija listi za pohranu podataka
-lista_vx, lista_vy, lista_x, lista_y, lista_vremena = [], [], [], [], []
-
-# Parametri simulacije
-x, y, t, dt = 0, 0, 0, 0.01
-simulacija_trajanje = 10
-
-# Simulacija gibanja
-while t <= simulacija_trajanje:
+while t <= 10:
     lista_vremena.append(t)
-    lista_vx.append(vx)
-    lista_vy.append(vy)
-    lista_x.append(x)
-    lista_y.append(y)
-
-    # Ažuriranje brzina i pozicija koristeći jednadžbe gibanja
     vx = vx + ax * dt
+    lista_vx.append(vx)
     vy = vy + ay * dt
+    lista_vy.append(vy)
     x = x + vx * dt
+    lista_x.append(x)
     y = y + vy * dt
-
+    lista_y.append(y)
     t = t + dt
 
-# Crtanje grafova
 plt.subplot(1, 3, 1)
 plt.plot(lista_x, lista_y)
-plt.title('x-y Graf')
-plt.xlabel('x (m)')
-plt.ylabel('y (m)')
+plt.title("x-y graf")
+plt.xlabel("x [m]")
+plt.ylabel("y [m]")
 
 plt.subplot(1, 3, 2)
 plt.plot(lista_vremena, lista_x)
-plt.title('x-t Graf')
-plt.xlabel('Vrijeme (s)')
-plt.ylabel('x (m)')
+plt.title("x-t graf")
+plt.xlabel("Vrijeme [s]")
+plt.ylabel("x [m]")
 
 plt.subplot(1, 3, 3)
 plt.plot(lista_vremena, lista_y)
-plt.title('y-t Graf')
-plt.xlabel('Vrijeme (s)')
-plt.ylabel('y (m)')
+plt.title("y-t graf")
+plt.xlabel("Vrijeme [s]")
+plt.ylabel("y [m]")
 
-plt.tight_layout()
 plt.show()
