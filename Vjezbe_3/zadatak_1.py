3ef60cf58bef061dd9f9c0c7e26a30c1c53c7280
anteamarovic <amarovic1@pmfst.hr>
2024-03-14 11:19:11 +0100

    commit

--- Vjezbe_3/zadatak_1.py
+++ Vjezbe_3/zadatak_1.py
@@ -2,7 +2,9 @@
 a=5.0
 b=4.935
 print(a-b)
-#
+#Ocekujem rezultat '0.065', a dobivam rezultat '0.06500000000000039', mi ljudi koristimo dekadski brojevni sustav 
+#dok racunala koriste binarni brojevni sustav(tj. imaju samo stanja 0 i 1) zbog toga racunala ne mogu zapisati odredene brojeve(npr.0.1,0.2,...)
+#te nastaju aproksimacije i pogreske
 
 #zadatak b)
 a=0.1
