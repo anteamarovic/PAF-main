05e5eddd8bc55aec1d26593de06aecf36d8896b6
anteamarovic <amarovic1@pmfst.hr>
2024-04-12 13:31:49 +0200

    commit

--- Vjezbe_5/zadatak_2.py
+++ Vjezbe_5/zadatak_2.py
@@ -1,30 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Definiraj funkciju
 def funkcija(x):
     return np.sin(x)
 
-# Definiraj funkciju za derivaciju
 def derivacija(f, x, dx=1e-6):
     return (f(x + dx) - f(x - dx)) / (2 * dx)
 
-# Definiraj vrijednosti x
-x = np.linspace(-5, 5, 1000)
+x = np.linspace(-5, 5, 100)
 
-# Izračunaj derivaciju
 y = derivacija(funkcija, x)
 
-# Prikaži derivaciju
 plt.plot(x, y, label='Derivacija funkcije sin(x)')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
 
-# Analitičko rješenje
 y_analiticko = np.cos(x)
 
-# Prikaži numeričko i analitičko rješenje
 plt.plot(x, y, label='Numeričko rješenje')
 plt.plot(x, y_analiticko, label='Analitičko rješenje')
 plt.xlabel('x')
