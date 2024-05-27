29b87505a4d64a497e4011cf76d1647b1ebfe573
anteamarovic <amarovic1@pmfst.hr>
2024-05-27 10:27:07 +0200

    commit

---
+++ seminar/zadatak.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+def provjeri_poziciju_tocke(tocka, ishodiste, radijus, spremi_sliku=False, ime_slike=None):
+    # Računanje udaljenosti
+    udaljenost = np.linalg.norm(np.array(tocka) - np.array(ishodiste))
+    
+    # Provjera položaja točke
+    if udaljenost < radijus:
+        pozicija = "unutar"
+    elif udaljenost == radijus:
+        pozicija = "na"
+    else:
+        pozicija = "izvan"
+    
+    # Crtanje kruga i točke
+    fig, ax = plt.subplots()
+    krug = plt.Circle(ishodiste, radijus, edgecolor='blue', facecolor='none')
+    ax.add_patch(krug)
+    ax.plot(tocka[0], tocka[1], 'ro', label='Točka')
+    ax.set_aspect('equal', adjustable='datalim')
+    ax.legend()
+    
+    # Ispis rezultata
+    print(f"Točka je {pozicija} kruga i udaljena je {abs(udaljenost - radijus):.2f} jedinica.")
+    
+    # Prikazivanje ili spremanje slike
+    if spremi_sliku:
+        if ime_slike:
+            plt.savefig(f"{ime_slike}.png")
+        else:
+            plt.savefig("pozicija_tocke_u_krugu.png")
+    else:
+        plt.show()
+
+# Primjeri korištenja
+provjeri_poziciju_tocke([2, 3], [0, 0], 5, spremi_sliku=True, ime_slike="slika_kruga")
+provjeri_poziciju_tocke([6, 8], [0, 0], 5)
+provjeri_poziciju_tocke([3, 4], [0, 0], 5, spremi_sliku=True, ime_slike="kruznica2")
+provjeri_poziciju_tocke([-2, -3], [0, 0], 5)
